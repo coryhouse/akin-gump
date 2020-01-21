0ec02ab2903348dd2d432d95a29fd9293456a1ae
Cory House <housecor@gmail.com>
2020-01-21 16:54:56 -0500

    Add form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // useState lets us declare state that changes over time
 import React, { useState } from "react";
+import Input from "./Input";
 
 const defaultUsers = [
   { id: 1, name: "Cory", email: "c@h.com", role: "Admin" },
@@ -7,14 +8,33 @@ const defaultUsers = [
   { id: 3, name: "Frey", email: "frey@ag.com", role: "Admin" }
 ];
 
+const emptyUser = {
+  name: "",
+  email: "",
+  role: ""
+};
+
 function App() {
   // Declare state because this info changes over time
   // and we want React to redraw the screen when this data changes.
   const [users, setUsers] = useState(defaultUsers);
+  const [user, setUser] = useState(emptyUser);
 
   function deleteUser(userId) {
     const newUsers = users.filter(user => user.id !== userId);
     setUsers(newUsers);
+  }
+
+  // Using computed property to reference a property via a variable
+  function onChange({ target }) {
+    const { name, value } = target;
+    setUser({ ...user, [name]: value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // create a copy of users array, and add new element to it.
+    setUsers([...users, user]);
   }
 
   function renderUser(user) {
@@ -41,24 +61,30 @@ function App() {
       {/* In JSX, you must comment using this style */}
       <h1>Users</h1>
       <h2>Add User</h2>
-      <form>
-        <div>
-          <label htmlFor="name">Name</label>
-          <br />
-          <input id="name" type="text"></input>
-        </div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Name"
+          id="name"
+          name="name"
+          onChange={onChange}
+          value={user.name}
+        />
 
-        <div>
-          <label htmlFor="email">Email</label>
-          <br />
-          <input id="email" type="text"></input>
-        </div>
+        <Input
+          label="Email"
+          id="email"
+          name="email"
+          onChange={onChange}
+          value={user.email}
+        />
 
-        <div>
-          <label htmlFor="role">Role</label>
-          <br />
-          <input id="role" type="text"></input>
-        </div>
+        <Input
+          label="Role"
+          id="role"
+          name="role"
+          onChange={onChange}
+          value={user.role}
+        />
 
         <input type="submit" value="Add User" />
       </form>
